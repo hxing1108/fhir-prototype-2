5141fc879bbfb21159dc6087ea1c753f8351e7cb
hxing1108 <hxing1108@users.noreply.github.com>
2025-06-17 17:12:36 +0200

    Update notification dot color in PMS integration panel

--- src/components/properties-sidebar/pms-integration/PMSIntegrationPanel.tsx
+++ src/components/properties-sidebar/pms-integration/PMSIntegrationPanel.tsx
@@ -95,7 +95,7 @@ export const PMSIntegrationPanel: React.FC<PMSIntegrationPanelProps> = ({
                   savedContent[`takeover_${selectedElementId}`] && (
                     <div
                       className="absolute -top-1 -right-1 w-3 h-3 rounded-full"
-                      style={{ backgroundColor: '#DEE9FC' }}
+                      style={{ backgroundColor: '#78AAFF' }}
                     ></div>
                   )}
               </div>
